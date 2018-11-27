9ed7eba8585e9ec4c11154daea5672114abf58fe
Kees Hink <keeshink@gmail.com>
2018-11-27 19:16:56 +0100

    Fix flake8 errors

--- strava_make_all_private.py
+++ strava_make_all_private.py
@@ -1,5 +1,4 @@
 from __future__ import print_statement
-import time
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
@@ -9,10 +8,12 @@ swagger_client.configuration.access_token = 'YOUR_ACCESS_TOKEN'
 
 # create an instance of the API class
 api_instance = swagger_client.ActivitiesApi()
-before = 56 # Integer | An epoch timestamp to use for filtering activities that have taken place before a certain time. (optional)
-after = 56 # Integer | An epoch timestamp to use for filtering activities that have taken place after a certain time. (optional)
-page = 56 # Integer | Page number. (optional)
-perPage = 56 # Integer | Number of items per page. Defaults to 30. (optional) (default to 30)
+# Integer | An epoch timestamp to use for filtering activities that have taken place before a certain time. (optional)
+before = 56
+# Integer | An epoch timestamp to use for filtering activities that have taken place after a certain time. (optional)
+after = 56
+page = 56  # Integer | Page number. (optional)
+perPage = 56  # Integer | Number of items per page. Defaults to 30. (optional) (default to 30)
 
 try:
     # List Athlete Activities
